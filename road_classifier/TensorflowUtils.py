6c9e1f72104827986ef4aef018b06ef65de8c5d2
Steven Daniluk <stevendaniluk8@gmail.com>
2016-10-30 14:32:45 -0400

    Improve batch normalization and bottleneck

--- road_classifier/TensorflowUtils.py
+++ road_classifier/TensorflowUtils.py
@@ -21,18 +21,20 @@ def variable_summaries(var, name):
     tf.histogram_summary(name, var)
 
 # Create a weight variable with appropriate initialization
-def weight_variable(shape, name = None):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  if name is not None:
-  	variable_summaries(initial, name + "/weights")
-  return tf.Variable(initial)
+def weight_variable(shape, name=None):
+	with tf.name_scope('weights'):
+	  initial = tf.truncated_normal(shape, stddev=0.1)
+	  if name is not None:
+	  	variable_summaries(initial, name + "/weights")
+	  return tf.Variable(initial)
 
 # Create a bias variable with appropriate initialization
 def bias_variable(shape, name = None):
-  initial = tf.constant(0.0, shape=shape)
-  if name is not None:
-  	variable_summaries(initial, name + "/biases")
-  return tf.Variable(initial)
+	with tf.name_scope('biases'):
+	  initial = tf.constant(0.0, shape=shape)
+	  if name is not None:
+	  	variable_summaries(initial, name + "/biases")
+	  return tf.Variable(initial)
 
 # Create a convolution layer
 def conv2d(x, W, b, stride):
@@ -57,107 +59,88 @@ def max_pool_2x2(x):
 def subsample(x, factor):
   return tf.nn.max_pool(x, ksize=[1, factor, factor, 1], strides=[1, 1, 1, 1], padding="SAME")
 
-# Normalize a batch
-def batch_norm(x, n_out, training, scope='batch_norm'):
-	# Code taken from: http://stackoverflow.com/a/34634291/2267819
-
-  """
-  Batch normalization on convolutional maps.
-  Args:
-    x:           Tensor, 4D BHWD input maps
-    n_out:       Integer, depth of input maps
-    training:    boolean tf.Varialbe, true indicates training phase
-    scope:       string, variable scope
-  Return:
-    normed:      batch-normalized maps
-  """
-  with tf.variable_scope(scope):
-    beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
-                                 name='beta', trainable=True)
-    gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
-                                  name='gamma', trainable=True)
-    batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
-    ema = tf.train.ExponentialMovingAverage(decay=0.5)
-
-    def mean_var_with_update():
-        ema_apply_op = ema.apply([batch_mean, batch_var])
-        with tf.control_dependencies([ema_apply_op]):
-            return tf.identity(batch_mean), tf.identity(batch_var)
-
-    mean, var = tf.cond(training,
-                        mean_var_with_update,
-                        lambda: (ema.average(batch_mean), ema.average(batch_var)))
-    normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
-  return normed
+# Batch normalization
+def batch_norm(inputT, is_training=True, scope=None):
+	# Scope is mandatory
+	# Variables are updated one layer at a time (not the most efficient, but more convenient)
+  return tf.cond(is_training, 
+  	             lambda: tf.contrib.layers.batch_norm(inputT, is_training=True, updates_collections=None, center=False, scope=scope, reuse = False), 
+  	             lambda: tf.contrib.layers.batch_norm(inputT, is_training=False, updates_collections=None, center=False, scope=scope, reuse = True))
 
 # Bottleneck unit
-def bottleneck(x, k, rate, training, name = None):
+def bottleneck(x, k, rate, training, name=None, summaries=False):
 	"""
+	Bottleneck unit consists of the following layers:
+	  -1x1 convolution to reduce dimensionality (halfed)
+	  -kxk convolution at reduced dimensionality
+	  -1x1 convolution to increase dimensionality
+	  -Skip connection from the input before activation
+
 	Args:
     x:           Tensor - 4D BHWD input
     k:           Integer - [W, H] of kernel
     rate:        Integer - Rate aprameter for dilated convolution
     training:    Bool - Training or inference
+    summaries:   Bool - Record summaries
+    name:        String - Layer name for scoping
   Return:
-    normed:      batch-normalized maps
+    out:         Output tensor, same dimensions as x
   """
 
 	in_shape = x.get_shape()
+	ch_in = in_shape[3].value
+	ch_reduced = ch_in/2
 
-	with tf.name_scope("branch_a"):
-		# Subsample input by a factor of 2
-	  a = subsample(x, 2)
+	# Convolution to reduce dimensionality 
+	with tf.name_scope('b1'):
+		if summaries:
+			local_name = name + "/b1"
+		else:
+			local_name = None
 
-	with tf.name_scope("branch_b"):
-		# Convolution to reduce dimensionality 
-		with tf.name_scope('b1'):
-			if name is not None:
-				local_name = name + "/b1"
-			else:
-				local_name = name
+		with tf.name_scope('weights'):
+		  W = weight_variable([1, 1, ch_in, ch_reduced], local_name)
+		with tf.name_scope('biases'):
+		  b = bias_variable([ch_reduced], local_name)
+		conv = conv2d(x, W, b, 1)
+		b1 = tf.nn.tanh(conv)
 
-			with tf.name_scope('weights'):
-			  W = weight_variable([1, 1, in_shape[3].value, 1], local_name)
-			with tf.name_scope('biases'):
-			  b = bias_variable([1], local_name)
-			conv = conv2d(x, W, b, 1)
-			b1 = tf.nn.tanh(conv)
+	# Convolution with kernel
+	with tf.name_scope('b2'):
+		if summaries:
+			local_name = name + "/b2"
+		else:
+			local_name = None
 
-		# Convolution with kernel
-		with tf.name_scope('b2'):
-			if name is not None:
-				local_name = name + "/b2"
-			else:
-				local_name = name
+		with tf.name_scope('weights'):
+		  W = weight_variable([k[0], k[1], ch_reduced, ch_reduced], local_name)
+		with tf.name_scope('biases'):
+		  b = bias_variable([ch_reduced], local_name)
 
-			with tf.name_scope('weights'):
-			  W = weight_variable([k[0], k[1], 1, 1], local_name)
-			with tf.name_scope('biases'):
-			  b = bias_variable([1], local_name)
-			if rate == 1:
-				conv = conv2d(b1, W, b, 1)
-			else:
-				conv = conv2d_dilated(b1, W, b, rate)
-			b2 = tf.nn.tanh(conv)
+		if rate == 1:
+			conv = conv2d(b1, W, b, 1)
+		else:
+			conv = conv2d_dilated(b1, W, b, rate)
+		b2 = tf.nn.tanh(conv)
 
-		# Convolution to expand dimensionality
-		with tf.name_scope('b3'):
-			if name is not None:
-				local_name = name + "/b3"
-			else:
-				local_name = name
+	# Convolution to expand dimensionality
+	with tf.name_scope('b3'):
+		if summaries:
+			local_name = name + "/b3"
+		else:
+			local_name = None
 
-			with tf.name_scope('weights'):
-			  W = weight_variable([1, 1, 1, in_shape[3].value], local_name)
-			with tf.name_scope('biases'):
-			  b = bias_variable([in_shape[3].value], local_name)
-			b3 = conv2d(b2, W, b, 1)
+		with tf.name_scope('weights'):
+		  W = weight_variable([1, 1, ch_reduced, ch_in], local_name)
+		with tf.name_scope('biases'):
+		  b = bias_variable([ch_in], local_name)
+		b3 = conv2d(b2, W, b, 1)
 
-		# Normalization
-		with tf.name_scope('b4'):
-			b4 = batch_norm(b3, in_shape[3].value, training)
+	# Normalization
+	with tf.variable_scope(name + "/norm") as scope:
+		b4 = batch_norm(b3, is_training=training, scope=scope)
 
-	with tf.name_scope('prelu_out'):
-	  out = tf.nn.tanh(tf.add(a, b4))
+	with tf.name_scope('out'):
+	  out = tf.add(x, b4)
 
 	return out
